131ef327a3dfe7683535110d6e389d471352d2c8
cho4u4o <cho4u4o@gmail.com>
2025-07-08 17:45:16 +0900

    feat : implement initial login page

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -9,12 +9,12 @@
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
 import { Route as rootRouteImport } from './routes/__root'
-import { Route as TestRouteImport } from './routes/test'
+import { Route as LoginRouteImport } from './routes/login'
 import { Route as IndexRouteImport } from './routes/index'
 
-const TestRoute = TestRouteImport.update({
-  id: '/test',
-  path: '/test',
+const LoginRoute = LoginRouteImport.update({
+  id: '/login',
+  path: '/login',
   getParentRoute: () => rootRouteImport,
 } as any)
 const IndexRoute = IndexRouteImport.update({
@@ -25,37 +25,37 @@ const IndexRoute = IndexRouteImport.update({
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
-  '/test': typeof TestRoute
+  '/login': typeof LoginRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
-  '/test': typeof TestRoute
+  '/login': typeof LoginRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/': typeof IndexRoute
-  '/test': typeof TestRoute
+  '/login': typeof LoginRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/test'
+  fullPaths: '/' | '/login'
   fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/test'
-  id: '__root__' | '/' | '/test'
+  to: '/' | '/login'
+  id: '__root__' | '/' | '/login'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
-  TestRoute: typeof TestRoute
+  LoginRoute: typeof LoginRoute
 }
 
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
-    '/test': {
-      id: '/test'
-      path: '/test'
-      fullPath: '/test'
-      preLoaderRoute: typeof TestRouteImport
+    '/login': {
+      id: '/login'
+      path: '/login'
+      fullPath: '/login'
+      preLoaderRoute: typeof LoginRouteImport
       parentRoute: typeof rootRouteImport
     }
     '/': {
@@ -70,7 +70,7 @@ declare module '@tanstack/react-router' {
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
-  TestRoute: TestRoute,
+  LoginRoute: LoginRoute,
 }
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
---
+++ src/routes/login.tsx
@@ -0,0 +1,31 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+export const Route = createFileRoute('/login')({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div className='grid size-full place-items-center'>
+      <div className='flex flex-col items-center gap-8'>
+        <div className='h-[100px] w-[300px] overflow-hidden object-fill'>
+          <DotLottieReact
+            src='https://lottie.host/3efa4d60-e962-4ae8-abad-00fa3c1284e9/StykaE4rpd.lottie'
+            loop
+            autoplay
+            style={{ scale: 3 }}
+          />
+        </div>
+        <span className='text-[48px] font-black md:text-[64px]'>yummmjin</span>
+        <button className='cursor-pointer rounded-full border-2 bg-black px-8 py-2 text-xl text-white transition duration-300 outline-none hover:border-gray-300 hover:bg-white hover:text-black'>
+          시작하기
+        </button>
+        <p className='text-center text-4xl leading-tight'>
+          작은 코드,
+          <br />큰 가능성.
+        </p>
+      </div>
+    </div>
+  );
+}
