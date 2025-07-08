be2803ca1201767af976b4e2eabe4dc6d369edcc
cho4u4o <cho4u4o@gmail.com>
2025-07-08 16:06:00 +0900

    initial commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,14 @@
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+
+import Header from '../components/Header'
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <Header />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+})
