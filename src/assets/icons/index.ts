4978a36598f6440458404ed3f80b7cbbff9851e4
cho4u4o <cho4u4o@gmail.com>
2025-07-08 23:16:04 +0900

    feat : add icons for home screen

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,17 @@
-import Logo from './logo.png'
-import PlusButton from './icon-plus.svg'
-import MenuButton from './icon-menu.svg'
+import Logo from './logo.png';
+import PlusButton from './icon-plus.svg';
+import MenuButton from './icon-menu.svg';
+import CodeIcon from './icon-code.svg';
+import ProjectIcon from './icon-project.svg';
+import HackathonIcon from './icon-hackathon.svg';
+import OrganizationIcon from './icon-org.svg';
 
-export { Logo, PlusButton, MenuButton }
+export {
+  Logo,
+  PlusButton,
+  MenuButton,
+  CodeIcon,
+  ProjectIcon,
+  HackathonIcon,
+  OrganizationIcon,
+};
