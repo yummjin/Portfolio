bde7fde91113a962036a9f03753ad5e491a98597
cho4u4o <cho4u4o@gmail.com>
2025-07-08 17:53:15 +0900

    chore : add `type/node` to solve type error and delete test settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,13 +12,9 @@ export default defineConfig({
     viteReact(),
     tailwindcss(),
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
     },
   },
-})
+});
