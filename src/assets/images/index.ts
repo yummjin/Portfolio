b318d59ff50b3d14e26f7439dd051b2063f28538
cho4u4o <cho4u4o@gmail.com>
2025-07-09 00:06:04 +0900

    feat : add images for profile

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,5 @@
 import Wallpaper1 from './wallpaper-1.jpg';
+import Profile from './profile.jpeg';
+import Profile2 from './profile2.jpeg';
 
-export { Wallpaper1 };
+export { Wallpaper1, Profile, Profile2 };
